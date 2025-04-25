b96ba7a5be30aaf77a84ad123700f24960a91346
Alex Tremblay <atremblay@ironflytechnologies.com>
2025-04-25 11:09:13 +1000

    Adding clean logo page

--- front/app/tailwind.config.js
+++ front/app/tailwind.config.js
@@ -8,8 +8,30 @@ module.exports = {
       // You can extend the default theme here
       // For example, add custom colors, fonts, etc.
       colors: {
-        primary: '#your-primary-color', // Example custom color
-        secondary: '#your-secondary-color', // Example custom color
+        primary: {
+          50: '#e6edf3',
+          100: '#ccdbe7',
+          200: '#99b7cf',
+          300: '#6693b7',
+          400: '#336f9f',
+          500: '#004b87',
+          600: '#003c6c',
+          700: '#002e51',
+          800: '#001f36',
+          900: '#114577',
+        },
+        secondary: {
+          50: '#e6f5ec',
+          100: '#ccead9',
+          200: '#99d5b3',
+          300: '#66c18d',
+          400: '#33ac67',
+          500: '#009741',
+          600: '#007934',
+          700: '#005c28',
+          800: '#003e1b',
+          900: '#00732b',
+        },
       },
     },
     screens: {
@@ -22,5 +44,10 @@ module.exports = {
   },
   plugins: [
     // Add any Tailwind plugins here, e.g., require('@tailwindcss/forms')
+    function({ addBase }) {
+      addBase({
+        'button': { cursor: 'pointer' },
+      });
+    },
   ],
 }
